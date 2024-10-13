e00b3acbbe33d56726128c0ea3b73bfbae874710
Владимир Экизян <9885313888va@gmail.com>
2024-10-13 11:41:51 +0300

    Изменения

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  Print('Hello World!');
+  var language:= ReadString('What language do you speak?');
+  case language of
+    'English': Print('Hello!');
+    'Español': Print('Hola!');
+    '中文': Print('你好！');
+    'Русский': Print('Здравствуйте!');
+    'Français': Print('Bonjour!');
+  end;
 end.
